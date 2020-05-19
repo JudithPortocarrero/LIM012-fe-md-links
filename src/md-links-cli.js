fc3d2a6cffb56d2e1b3c9e8cb5afeecdd83408f8
unknown <judithportocarrero99@gmail.com>
2020-05-19 15:27:30 -0500

    package.json

--- src/md-links-cli.js
+++ src/md-links-cli.js
@@ -1,4 +1,3 @@
-// import { cliFunction } from './cli.js';
 const { cliFunction } = require('./cli.js');
 const input = [];
 for (let i = 3; i < process.argv.length; i += 1) {
@@ -6,4 +5,4 @@ for (let i = 3; i < process.argv.length; i += 1) {
 }
 const newInput = input.join(' ');
 const path = process.argv[2];
-cliFunction(path, newInput).then(res => console.log(res)).catch(error => console.error())
+cliFunction(path, newInput).then(res => console.log(res)).catch(error => console.error());
