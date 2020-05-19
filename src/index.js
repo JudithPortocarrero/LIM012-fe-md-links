91e4d08e48c6674be8fd03e1dd4edf5df86eacac
unknown <judithportocarrero99@gmail.com>
2020-05-19 18:39:00 -0500

    funcion md-links

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-// promesas y callback
 const { guardarLinks} = require('./utilities.js');
 const { validateLinks } = require('./validate.js');
-const mdLinks = (ruta, options) => { //ruta = 'C:/Users/PORTOCARRERO/Desktop/LIM012-fe-md-links/carpetaPrueba & options = {validate: true}
+const mdLinks = (ruta, options) => { 
   return promesa = new Promise((resolve) => {
     if ( options.validate === true) {
       resolve(validateLinks(ruta));
@@ -12,9 +11,3 @@ const mdLinks = (ruta, options) => { //ruta = 'C:/Users/PORTOCARRERO/Desktop/LIM
   });
 }
 module.exports = {mdLinks};
-// Configuración de npm scripts.
-// Testeo asíncrono.	
-// Uso de librerias de Mock.	
-// Uso de Mocks manuales.	
-// Testeo para múltiples Sistemas Operativos.
-// Colaboración en Github (branches | pull requests | |tags).
